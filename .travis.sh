338355deddeedfb86ea2a9d6b274c2467652e395
jeroendedauw <jeroendedauw@gmail.com>
2013-06-13 14:33:59 +0200

    Move travis setup to dedicated sh file and make sqlite non-optional
    Change-Id: Idfb0be03885c462a0d4a4a24ebf147b976d856f4

---
+++ .travis.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd ..
+pwd
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
+cd phase3
+mysql -e 'create database its_a_mw;'
+php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
+cd extensions
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Diff.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataValues.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Ask.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseQueryEngine.git
+cd WikibaseQueryEngine
