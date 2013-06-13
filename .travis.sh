9bc0b9cc3de4ff342b6bb61c1b887e341ee9f245
jeroendedauw <jeroendedauw@gmail.com>
2013-06-13 19:07:46 +0200

    Have the tests run both as stanalone and with MediaWiki loaded on TravisCI
    Change-Id: If4053bc1de14f9288fd0f7910165bdc287b2cade

--- .travis.sh
+++ .travis.sh
@@ -2,15 +2,23 @@
 
 set -x
 
-cd ..
-pwd
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
-cd phase3
-mysql -e 'create database its_a_mw;'
-php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
-cd extensions
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Diff.git
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataValues.git
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Ask.git
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseQueryEngine.git
+if ["$STANALONE" -eq "yes"]
+then
+	composer install
+	phpunit --testsuite=QueryEngine
+else
+	cd ..
+	pwd
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
+	cd phase3
+	mysql -e 'create database its_a_mw;'
+	php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
+	cd extensions
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Diff.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataValues.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Ask.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseQueryEngine.git
+	cd WikibaseQueryEngine
+	phpunit
+fi
