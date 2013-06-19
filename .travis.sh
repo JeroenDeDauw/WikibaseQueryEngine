eb23981725530d465e4c140e928eab8b457554d8
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-06-19 12:22:21 +0000

    Revert "Improve TravisCI build script"
    This reverts commit 90433be5ce09f5d56dc21c32d2fe6d6167c46ac3
    
    Change-Id: I26227bee8b87884c055eca0ef9462dc7bb24ade5

--- .travis.sh
+++ .travis.sh
@@ -8,17 +8,19 @@ then
 	phpunit --testsuite=QueryEngine
 else
 	cd ..
+	pwd
 	git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
-
 	cd phase3
 	mysql -e 'create database its_a_mw;'
-
-	cd extensions
-	composer create-project wikibase/query-engine:dev-master WikibaseQueryEngine --keep-vcs
-
-	cd ..
 	php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
-
-	cd extensions/WikibaseQueryEngine
+	cd extensions
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Diff.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataValues.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Ask.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseDataModel.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseDatabase.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseQueryEngine.git
+	cd WikibaseQueryEngine
 	phpunit
 fi
