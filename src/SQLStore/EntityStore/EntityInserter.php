3836a64681d703417d86f108d1f5c6a8ffae3703
jeroendedauw <jeroendedauw@gmail.com>
2014-05-09 15:47:37 +0200

    Re-add unique index and filter duplicate main snak claims

--- src/SQLStore/EntityStore/EntityInserter.php
+++ src/SQLStore/EntityStore/EntityInserter.php
@@ -3,6 +3,8 @@
 namespace Wikibase\QueryEngine\SQLStore\EntityStore;
 
 use Doctrine\DBAL\Connection;
+use Traversable;
+use Wikibase\DataModel\Claim\Claim;
 use Wikibase\DataModel\Claim\Claims;
 use Wikibase\DataModel\Entity\Entity;
 use Wikibase\DataModel\Entity\PropertyId;
@@ -43,29 +45,109 @@ class EntityInserter {
 	}
 
 	private function insertStandardClaims() {
-		$propertyIds = array();
+		$claims = new ClaimList( $this->entity->getClaims() );
 
-		foreach ( $this->entity->getClaims() as $claim ) {
-			$propertyIds[$claim->getPropertyId()->getSerialization()] = true;
-		}
-
-		foreach ( $propertyIds as $propertyId => $true ) {
-			$claims = new Claims( $this->entity->getClaims() );
-			$this->insertClaims( $claims->getClaimsForProperty( new PropertyId( $propertyId ) )->getBestClaims() );
-		}
+		$this->insertClaims( $claims->getBestClaims()->getWithUniqueMainSnaks() );
 	}
 
 	private function insertVirtualClaims() {
 		// TODO: obtain and insert virtual claims
 	}
 
-	private function insertClaims( Claims $claims ) {
+	private function insertClaims( Traversable $claims ) {
 		foreach ( $claims as $claim ) {
 			$this->claimInserter->insertClaim(
 				$claim,
 				$this->entity->getId()
 			);
 		}
+	}
+
+}
+
+/**
+ * TODO: move to DM
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ClaimList implements \IteratorAggregate {
+
+	/**
+	 * @var Claim[]
+	 */
+	private $claims;
+
+	/**
+	 * @param Claim[] $claims
+	 */
+	public function __construct( array $claims = array() ) {
+		$this->claims = array_values( $claims );
+	}
+
+	/**
+	 * Returns the best claims.
+	 * The best claims are those with the highest rank for a particular property.
+	 * Deprecated ranks are never included.
+	 *
+	 * Caution: the ranking is done per property, not globally, as in the Claims class.
+	 *
+	 * @return self
+	 */
+	public function getBestClaims() {
+		$claimList = new self();
+
+		foreach ( $this->getPropertyIds() as $propertyId ) {
+			$claims = new Claims( $this->claims );
+			$claimList->addClaims( $claims->getClaimsForProperty( $propertyId )->getBestClaims() );
+		}
+
+		return $claimList;
+	}
+
+	/**
+	 * @return PropertyId[]
+	 */
+	public function getPropertyIds() {
+		$propertyIds = array();
+
+		foreach ( $this->claims as $claim ) {
+			$propertyIds[$claim->getPropertyId()->getSerialization()] = $claim->getPropertyId();
+		}
+
+		return array_values( $propertyIds );
+	}
+
+	private function addClaims( Claims $claims ) {
+		foreach ( $claims as $claim ) {
+			$this->addClaim( $claim );
+		}
+	}
+
+	private function addClaim( Claim $claim ) {
+		$this->claims[] = $claim;
+	}
+
+	/**
+	 * Claims that have a main snak already in the list are filtered out.
+	 * The last occurrences are retained.
+	 *
+	 * @return self
+	 */
+	public function getWithUniqueMainSnaks() {
+		$claims = array();
+
+		foreach ( $this->claims as $claim ) {
+			$claims[$claim->getMainSnak()->getHash()] = $claim;
+		}
+
+		return new self( $claims );
+	}
+
+	/**
+	 * @return Traversable
+	 */
+	public function getIterator() {
+		return new \ArrayIterator( $this->claims );
 	}
 
 }
