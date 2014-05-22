ff3eb220e1cdf8aa99f7121b995577b3345c54f8
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-05-22 14:32:52 +0200

    Moved calculation to DataValues/Time

--- src/SQLStore/DVHandler/TimeHandler.php
+++ src/SQLStore/DVHandler/TimeHandler.php
@@ -3,14 +3,11 @@
 namespace Wikibase\QueryEngine\SQLStore\DVHandler;
 
 use DataValues\DataValue;
-use DataValues\LatLongValue;
 use DataValues\TimeValue;
+use DataValues\TimeValueCalculator;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Types\Type;
 use InvalidArgumentException;
-use RuntimeException;
-use ValueParsers\CalendarModelParser;
-use ValueParsers\TimeParser;
 use Wikibase\QueryEngine\SQLStore\DataValueHandler;
 
 /**
@@ -21,12 +18,6 @@ use Wikibase\QueryEngine\SQLStore\DataValueHandler;
  * @author Thiemo Mättig
  */
 class TimeHandler extends DataValueHandler {
-
-	/**
-	 * Average length of a year in the Gregorian calendar.
-	 * 365 + 1 / 4 - 1 / 100 + 1 / 400 = 365.2425 days.
-	 */
-	const SECONDS_PER_YEAR = 31556952;
 
 	/**
 	 * @see DataValueHandler::getBaseTableName
@@ -40,13 +31,13 @@ class TimeHandler extends DataValueHandler {
 	 */
 	protected function completeTable( Table $table ) {
 		$table->addColumn( 'value', Type::STRING, array( 'length' => 33 ) );
-		$table->addColumn( 'value_epoche', Type::BIGINT );
-		$table->addColumn( 'value_epoche_min', Type::BIGINT );
-		$table->addColumn( 'value_epoche_max', Type::BIGINT );
+		$table->addColumn( 'value_timestamp', Type::BIGINT );
+		$table->addColumn( 'value_min_timestamp', Type::BIGINT );
+		$table->addColumn( 'value_max_timestamp', Type::BIGINT );
 
-		$table->addIndex( array( 'value_epoche' ) );
-		$table->addIndex( array( 'value_epoche_min' ) );
-		$table->addIndex( array( 'value_epoche_max' ) );
+		$table->addIndex( array( 'value_timestamp' ) );
+		$table->addIndex( array( 'value_min_timestamp' ) );
+		$table->addIndex( array( 'value_max_timestamp' ) );
 	}
 
 	/**
@@ -60,14 +51,14 @@ class TimeHandler extends DataValueHandler {
 	 * @see DataValueHandler::getEqualityFieldName
 	 */
 	public function getEqualityFieldName() {
-		return 'value_epoche';
+		return 'value_timestamp';
 	}
 
 	/**
 	 * @see DataValueHandler::getSortFieldName
 	 */
 	public function getSortFieldName() {
-		return 'value_epoche';
+		return 'value_timestamp';
 	}
 
 	/**
@@ -91,23 +82,24 @@ class TimeHandler extends DataValueHandler {
 	 *
 	 * @param DataValue $value
 	 *
-	 * @return array
 	 * @throws InvalidArgumentException
+	 * @return array
 	 */
 	public function getInsertValues( DataValue $value ) {
 		if ( !( $value instanceof TimeValue ) ) {
 			throw new InvalidArgumentException( 'Value is not a TimeValue' );
 		}
 
-		$epoche = $this->getEpoche( $value->getTime(), $value->getTimezone() );
+		$calculator = new TimeValueCalculator();
+		$timestamp = $calculator->getTimestamp( $value );
 		$before = $value->getBefore();
 		// The range from before to after must be at least one unit long
 		$after = max( 1, $value->getAfter() );
-		$precisionInSeconds = $this->getPrecisionInSeconds( $value->getPrecision() );
+		$precisionInSeconds = $calculator->getSecondsForPrecision( $value->getPrecision() );
 		$values = array(
-			'value_epoche' => $epoche,
-			'value_epoche_min' => $epoche - $before * $precisionInSeconds,
-			'value_epoche_max' => $epoche + $after * $precisionInSeconds,
+			'value_timestamp' => $timestamp,
+			'value_min_timestamp' => $timestamp - $before * $precisionInSeconds,
+			'value_max_timestamp' => $timestamp + $after * $precisionInSeconds,
 
 			'value' => $this->getEqualityFieldValue( $value ),
 		);
@@ -116,117 +108,12 @@ class TimeHandler extends DataValueHandler {
 	}
 
 	/**
-	 * @param string $time
-	 * @param int $timezone in minutes
-	 *
-	 * @throws RuntimeException
-	 * @return float
-	 */
-	private function getEpoche( $time, $timezone ) {
-		// Validation is done in TimeValue. As long if we found enough numbers we are fine.
-		if ( !preg_match( '/([-+]?\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)\D+(\d+)/', $time, $matches )
-		) {
-			throw new RuntimeException( "Failed to parse time value $time." );
-		}
-		list( , $fullYear, $month, $day, $hour, $minute, $second ) = $matches;
-
-		// There is no year 0 but this is not the place to do validation. Assume -1.
-		if ( $fullYear < 0 ) {
-			$fullYear += 1;
-		}
-
-		// We use mktime only for the month, day and time calculation. Set the year to the smallest
-		// possible in the 1970-2038 range to be safe, even if it's 1901-2038 since PHP 5.1.0.
-		$year = $this->isLeapYear( $fullYear ) ? 1972 : 1970;
-
-		$defaultTimezone = date_default_timezone_get();
-		date_default_timezone_set( 'UTC' );
-		// With day/month set to 0 mktime would calculate the last day of the previous month/year.
-		// In the context of this calculation we must assume 0 means "start of the month/year".
-		$timestamp = mktime( $hour, $minute, $second, max( 1, $month ), max( 1, $day ), $year );
-		date_default_timezone_set( $defaultTimezone );
-
-		if ( $timestamp === false ) {
-			throw new RuntimeException( "Failed to get epoche from time value $time." );
-		}
-
-		$missingYears = $fullYear - $year;
-		$missingLeapDays = $this->getLeapDays( $fullYear ) - $this->getLeapDays( $year );
-
-		return $timestamp + ( $missingYears * 365 + $missingLeapDays ) * 86400 - $timezone * 60;
-	}
-
-	/**
-	 * @param float $year
-	 *
-	 * @return bool
-	 */
-	private function isLeapYear( $year ) {
-		$isMultipleOf4   = fmod( $year,   4 ) === 0.0;
-		$isMultipleOf100 = fmod( $year, 100 ) === 0.0;
-		$isMultipleOf400 = fmod( $year, 400 ) === 0.0;
-		return $isMultipleOf4 && !$isMultipleOf100 || $isMultipleOf400;
-	}
-
-	/**
-	 * @param float $year
-	 *
-	 * @return float
-	 */
-	private function getLeapDays( $year ) {
-		return floor( $year / 4 ) - floor( $year / 100 ) + floor( $year / 400 );
-	}
-
-	/**
-	 * @param int $precision
-	 *
-	 * @throws RuntimeException
-	 * @return float
-	 */
-	private function getPrecisionInSeconds( $precision ) {
-		switch ( $precision ) {
-			case TimeValue::PRECISION_SECOND:
-				return 1;
-			case TimeValue::PRECISION_MINUTE:
-				return 60;
-			case TimeValue::PRECISION_HOUR:
-				return 3600;
-			case TimeValue::PRECISION_DAY:
-				return 86400;
-			case TimeValue::PRECISION_MONTH:
-				return self::SECONDS_PER_YEAR / 12;
-			case TimeValue::PRECISION_YEAR:
-				return self::SECONDS_PER_YEAR;
-			case TimeValue::PRECISION_10a:
-				return self::SECONDS_PER_YEAR * 10;
-			case TimeValue::PRECISION_100a:
-				return self::SECONDS_PER_YEAR * 100;
-			case TimeValue::PRECISION_ka:
-				return self::SECONDS_PER_YEAR * 1000;
-			case TimeValue::PRECISION_10ka:
-				return self::SECONDS_PER_YEAR * 10000;
-			case TimeValue::PRECISION_100ka:
-				return self::SECONDS_PER_YEAR * 100000;
-			case TimeValue::PRECISION_Ma:
-				return self::SECONDS_PER_YEAR * 1000000;
-			case TimeValue::PRECISION_10Ma:
-				return self::SECONDS_PER_YEAR * 10000000;
-			case TimeValue::PRECISION_100Ma:
-				return self::SECONDS_PER_YEAR * 100000000;
-			case TimeValue::PRECISION_Ga:
-				return self::SECONDS_PER_YEAR * 1000000000;
-		}
-
-		throw new RuntimeException( "Unable to get seconds for precision $precision." );
-	}
-
-	/**
 	 * @see DataValueHandler::getEqualityFieldValue
 	 *
 	 * @param DataValue $value
 	 *
-	 * @return string
 	 * @throws InvalidArgumentException
+	 * @return string
 	 */
 	public function getEqualityFieldValue( DataValue $value ) {
 		if ( !( $value instanceof TimeValue ) ) {
--- tests/Phpunit/SQLStore/DVHandler/TimeHandlerTest.php
+++ tests/Phpunit/SQLStore/DVHandler/TimeHandlerTest.php
@@ -22,33 +22,6 @@ use Wikibase\QueryEngine\Tests\Phpunit\SQLStore\DataValueHandlerTest;
 class TimeHandlerTest extends DataValueHandlerTest {
 
 	/**
-	 * @param string $time
-	 * @param int $timezone in minutes
-	 *
-	 * @return \PHPUnit_Framework_MockObject_MockObject
-	 */
-	private function getTimeValueMock( $time, $timezone = 0 ) {
-		$timeValue = $this->getMockBuilder( 'DataValues\TimeValue' )
-			->disableOriginalConstructor()
-			->getMock();
-
-		$timeValue->expects( $this->any() )
-			->method( 'getTime' )
-			->will( $this->returnValue( $time ) );
-		$timeValue->expects( $this->any() )
-			->method( 'getTimezone' )
-			->will( $this->returnValue( $timezone ) );
-		$timeValue->expects( $this->any() )
-			->method( 'getPrecision' )
-			->will( $this->returnValue( TimeValue::PRECISION_DAY ) );
-		$timeValue->expects( $this->any() )
-			->method( 'getCalendarModel' )
-			->will( $this->returnValue( 'Stardate' ) );
-
-		return $timeValue;
-	}
-
-	/**
 	 * @see DataValueHandlerTest::getInstances
 	 *
 	 * @since 0.1
@@ -106,79 +79,20 @@ class TimeHandlerTest extends DataValueHandlerTest {
 	}
 
 	/**
-	 * @dataProvider epocheCalculationProvider
+	 * @dataProvider valueProvider
 	 *
-	 * @param string $time
-	 * @param int $expected
-	 * @param int $timezone in minutes
+	 * @param TimeValue $timeValue
 	 */
-	public function testEpocheCalculation( $time, $expected, $timezone = 0 ) {
+	public function testGetInsertValues( TimeValue $timeValue ) {
 		$instance = $this->newInstance();
 
-		$timeValue = $this->getTimeValueMock( $time, $timezone );
 		$insertValues = $instance->getInsertValues( $timeValue );
-		$epoche = $insertValues['value_epoche'];
 
-		$this->assertEquals( $expected, $epoche );
-	}
-
-	public function epocheCalculationProvider() {
-		return array(
-			// Make sure it's identical to the PHP/Unix time stamps in current years
-			array( '+2004-02-29T00:00:00Z', strtotime( '2004-02-29T00:00:00+00:00' ) ),
-			array( '+2038-00-00T00:00:00Z', strtotime( '2038-01-01T00:00:00+00:00' ) ),
-
-			// Time zones
-			array( '+2000-01-01T12:59:59', strtotime( '2000-01-01T12:59:59-02:00' ), -120 ),
-			array( '+2000-01-01T12:59:59', strtotime( '2000-01-01T12:59:59+04:45' ), 285 ),
-
-			array( '+0401-00-00T00:00:00Z', -49512816000 ),
-			array( '+1902-00-00T00:00:00Z', -2145916800 ),
-			array( '+1939-00-00T00:00:00Z', -978307200 ),
-			array( '+1969-12-31T23:59:00Z', -60 ),
-			array( '+1969-12-31T23:59:59Z', -1 ),
-			array( '+1970-01-01T00:00:00Z', 0 ),
-			array( '+1970-01-01T00:00:01Z', 1 ),
-			array( '+1970-01-01T00:01:00Z', 60 ),
-			array( '+1972-02-29T00:00:00Z', 68169600 ),
-			array( '+1996-02-29T00:00:00Z', 825552000 ),
-			array( '+1999-12-31T23:59:59Z', 946684799 ),
-			array( '+2000-01-01T00:00:00Z', 946684800 ),
-			array( '+2000-02-01T00:00:00Z', 949363200 ),
-			array( '+2000-02-29T00:00:00Z', 951782400 ),
-			array( '+2001-00-00T00:00:00Z', 978307200 ),
-			array( '+2001-01-01T00:00:00Z', 978307200 ),
-			array( '+2014-04-30T12:35:55Z', 1398861355 ),
-			array( '+2401-00-00T00:00:00Z', 13601088000 ),
-
-			// Make sure there is only 1 second between these two
-			array( '-0001-12-31T23:59:59Z', -62135596801 ),
-			array( '+0001-00-00T00:00:00Z', -62135596800 ),
-
-			// Year 0 does not exist, but we do not complain, assume -1
-			array( '-0000-12-31T23:59:59Z', -62135596801 ),
-			array( '+0000-00-00T00:00:00Z', floor( ( -1 - 1969 ) * 365.2425 ) * 86400 ),
-
-			// Since there is no year 0, negative leap years are -1, -5 and so on
-			array( '-8001-00-00T00:00:00Z', floor( ( -8001 - 1969 ) * 365.2425 ) * 86400 ),
-			array( '-0005-00-00T00:00:00Z', floor( ( -5 - 1969 ) * 365.2425 ) * 86400 ),
-			array( '+0004-00-00T00:00:00Z', floor( ( 4 - 1970 ) * 365.2425 ) * 86400 ),
-			array( '+8000-00-00T00:00:00Z', floor( ( 8000 - 1970 ) * 365.2425 ) * 86400 ),
-
-			// PHP_INT_MIN is -2147483648
-			array( '-2147484001-00-00T00:00:00Z', floor( ( -2147484001 - 1969 ) * 365.2425 ) * 86400 ),
-			// PHP_INT_MAX is +2147483647
-			array( '+2147484000-00-00T00:00:00Z', floor( ( 2147484000 - 1970 ) * 365.2425 ) * 86400 ),
-		);
-	}
-
-	public function testEqualityFieldValue() {
-		$instance = $this->newInstance();
-
-		$timeValue = $this->getTimeValueMock( '41153.7' );
-		$equalityFieldValue = $instance->getEqualityFieldValue( $timeValue );
-
-		$this->assertEquals( '41153.7|11|Stardate', $equalityFieldValue );
+		$this->assertInternalType( 'float', $insertValues['value_timestamp'] );
+		$this->assertInternalType( 'float', $insertValues['value_min_timestamp'] );
+		$this->assertInternalType( 'float', $insertValues['value_max_timestamp'] );
+		$this->assertLessThanOrEqual( $insertValues['value_timestamp'], $insertValues['value_min_timestamp'] );
+		$this->assertGreaterThan( $insertValues['value_timestamp'], $insertValues['value_max_timestamp'] );
 	}
 
 }
