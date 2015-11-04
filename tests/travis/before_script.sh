895409e73a2ed0c5db8240e04e527ae1b3b8bae2
jeroendedauw <jeroendedauw@gmail.com>
2015-11-04 20:24:38 +0100

    Try fix postgres on TravisCI

--- tests/travis/before_script.sh
+++ tests/travis/before_script.sh
@@ -9,6 +9,15 @@ fi
 
 if [ "$DB" = 'pgsql' ]
 then
+	sudo /etc/init.d/postgresql stop
+
+	# Travis@support: Try adding a sleep of a few seconds between starting PostgreSQL
+	# and the first command that accesses PostgreSQL
+	sleep 3
+
+	sudo /etc/init.d/postgresql start
+	sleep 3
+
 	psql -c 'create database qe_pg_tests_tmp;' -U postgres
 	psql -c 'create database qe_pg_tests;' -U postgres
 
